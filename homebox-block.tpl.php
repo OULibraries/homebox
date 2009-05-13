a88afd9c72c6172d078cdcd90d94447091cdce0c
Jérémy Chatard <jchatard@breek.fr>
2009-05-13 15:04:43 +0000

    Initial commit

---
+++ homebox-block.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+?>
+<div id="homebox-block-<?php print $block->module .'-'. $block->delta; ?>" class="<?php print $block->homebox_classes ?> clear-block block block-<?php print $block->module ?>">
+  <div class="homebox-portlet-inner">
+    <h3 class="portlet-header"><?php print $block->subject ?></h3>
+    <div class="portlet-config">
+      <?php if (variable_get('homebox_users_use_colors_'. $pid, FALSE)): ?>
+        <div class="homebox-colors">
+          <?php for ($i=0; $i < HOMEBOX_NUMBER_OF_COLOURS; $i++): ?>
+            <span href="#" class="homebox-color-selector" style="background-color: <?php print variable_get('homebox_color_'. $pid . '_' . $i, '#E4F0F8') ?>;">&nbsp;</span>
+          <?php endfor ?>
+        </div>
+      <?php endif ?>
+      <?php if ($block->module == 'views'): ?>
+        <?php print theme('homebox_views_exposed_filter', $block) ?>
+      <?php endif ?>
+      <?php if (variable_get('homebox_users_use_colors_'. $pid, FALSE) || $block->module == 'views'): ?>
+        <div class="clear-block"></div>
+      <?php endif ?>
+    </div>
+    <div class="portlet-content"><?php print $block->content ?></div>
+  </div>
+</div>
---
+++ homebox.tpl.php
@@ -0,0 +1,32 @@
+<?php
+// $Id$
+?>
+<div id="homebox" class="column-count-<?php print $column_count ?>">
+  
+  <a href="#" id="homebox-add"><?php print t('Add content') ?></a>
+  
+  <ul id="homebox-settings" class="homebox-settings-hidden">
+    <?php foreach ($available_blocks as $key => $block): ?>
+      <li>
+        <input type="checkbox" class="homebox_toggle_box" <?php print $block['checked'] ?> id="homebox_toggle_<?php print $block['dom_id'] ?>" /> <?php print $block['subject'] ?>
+      </li>
+    <?php endforeach ?>
+  </ul>
+  
+  <?php for ($i = 1; $i <= count($regions); $i++): ?>
+    <div class="homebox-column" id="homebox-column-<?php print $i ?>">
+      <?php foreach ($regions[$i] as $key => $block): ?>
+        <?php if ($block->content): ?>
+          <?php print theme('homebox_block', $block, $pid) ?>
+        <?php endif ?>
+      <?php endforeach ?>
+    </div>
+  <?php endfor ?>
+  
+  <div class="clear-block"></div>
+</div>
+
+<?php
+// Print CSS classes based on colors
+print $color_css_classes;
+?>
